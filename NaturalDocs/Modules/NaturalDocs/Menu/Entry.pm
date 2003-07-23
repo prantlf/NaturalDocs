d9a21e020938bec6514a3bd18920f8b5ec2c2ef9
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2003-07-23 00:44:49 +0000

    Big overhaul.  Too much to list.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk@78 6b081834-2649-4b4f-abbc-426aeab4333c

--- NaturalDocs/Modules/NaturalDocs/Menu/Entry.pm
+++ NaturalDocs/Modules/NaturalDocs/Menu/Entry.pm
@@ -25,22 +25,23 @@ package NaturalDocs::Menu::Entry;
 #
 #   The object is implemented as a blessed arrayref with the indexes below.
 #
-#       TYPE      - The type of entry.  Will be one of the <Menu Item Types>.
-#       TITLE     - The title of the entry.  If the type is <MENU_FILE>, undef means use the default menu title.  If the type is
-#                      <MENU_LINK>, undef means generate it from the URL itself.
+#       TYPE      - The type of entry.  Will be one of the <Menu Entry Types>.
+#       TITLE     - The title of the entry.
 #       TARGET  - The target of the entry.  If the type is <MENU_FILE>, it will be the source file name.  If the type is <MENU_LINK>,
 #                       it will be the URL.  If the type is <MENU_GROUP>, it will be an arrayref of <NaturalDocs::Menu::Entry>
-#                       objects representing the group's content, or undef if there are none.  If the type is <MENU_INDEX>, it will be
+#                       objects representing the group's content.  If the type is <MENU_INDEX>, it will be
 #                       one of the <Topic Types> or undef for the general index.
+#       FLAGS    - Any <Menu Entry Flags> that apply.
 #
 use constant TYPE => 0;
 use constant TITLE => 1;
 use constant TARGET => 2;
+use constant FLAGS => 3;
 # DEPENDENCY: New() depends on the order of these constants.
 
 
 ###############################################################################
-# Group: Functions
+# Group: Modification Functions
 
 #
 #   Function: New
@@ -49,28 +50,48 @@ use constant TARGET => 2;
 #
 #   Parameters:
 #
-#       type     - The type of the entry.  Must be one of the <Menu Item Types>.
-#       title      - The title of the entry.  If the type is <MENU_FILE>, set this to undef to use the default menu title.  If the type is
-#                     <MENU_LINK>, set this to undef to generate it from the URL itself.
+#       type     - The type of the entry.  Must be one of the <Menu Entry Types>.
+#       title      - The title of the entry.
 #       target   - The target of the entry, if applicable.  If the type is <MENU_FILE>, use the source file name.  If the type is
 #                     <MENU_LINK>, use the URL.  If the type is <MENU_INDEX>, use one of the <Topic Types> or undef for general.
 #                     Otherwise set it to undef.
+#       flags     - Any <Menu Entry Flags> that apply.
 #
-sub New #(type, title, target)
+sub New #(type, title, target, flags)
     {
     # DEPENDENCY: This gode depends on the order of the constants.
 
     my $object = [ @_ ];
     bless $object;
 
+    if ($object->[TYPE] == ::MENU_GROUP())
+        {  $object->[TARGET] = [ ];  };
+    if (!defined $object->[FLAGS])
+        {  $object->[FLAGS] = 0;  };
+
     return $object;
     };
+
+
+# Function: SetTitle
+# Replaces the entry's title.
+sub SetTitle #(title)
+    {  $_[0]->[TITLE] = $_[1];  };
+
+# Function: SetFlags
+# Replaces the <Menu Entry Flags>.
+sub SetFlags #(flags)
+    {  $_[0]->[FLAGS] = $_[1];  };
+
+
+###############################################################################
+# Group: Information Functions
 
 
 #
 #   Function: Type
 #
-#   Returns the type of the entry.  Will be one of the <Menu Item Types>.
+#   Returns the type of the entry.  Will be one of the <Menu Entry Types>.
 #
 sub Type
     {  return $_[0]->[TYPE];  };
@@ -81,19 +102,9 @@ sub Type
 #
 #   Returns the title of the entry.
 #
-#   The object will always return the correct title, so you don't have to worry about it being undef and applying the rules described
-#   in <New()>'s parameters.
-#
 sub Title
     {
-    my $self = shift;
-
-    if (defined $self->[TITLE])
-        {  return $self->[TITLE];  }
-    elsif ($self->Type() == ::MENU_FILE())
-        {  return NaturalDocs::Project::DefaultMenuTitleOf($self->Target());  }
-    elsif ($self->Type() == ::MENU_LINK())
-        {  return $self->Target();  };
+    return $_[0]->[TITLE];
     };
 
 
@@ -116,28 +127,27 @@ sub Target
 
 
 #
-#   Function: SpecifiesTitle
+#   Function: Flags
 #
-#   Returns whether the <MENU_FILE> or <MENU_LINK> specifies a title.  Note that <Title()> will always return the correct value;
-#   this is only for determining the setting.
+#   Returns the <Menu Entry Flags>.
 #
-sub SpecifiesTitle
+sub Flags
     {
-    return ( defined $_[0]->[TITLE] );
+    return $_[0]->[FLAGS];
     };
 
 
 ###############################################################################
 # Group: Group Functions
 #
-#   These functions all assume that the entry type is <MENU_GROUP>.  Do *not* call them without testing <Type()> first.
-#
+#   All of these functions assume the type is <MENU_GROUP>.  Do *not* call any of these without checking <Type()> first.
+
 
 #
 #   Function: GroupContent
 #
-#   Returns an arrayref of <NaturalDocs::Menu::Entry> objects representing the contents of the group, or undef if it is empty.
-#   Do not change the arrayref; use <PushToGroup()> and <PopFromGroup()> instead.
+#   Returns an arrayref of <NaturalDocs::Menu::Entry> objects representing the contents of the
+#   group, or undef otherwise.  This arrayref will always exist for <MENU_GROUP>'s and can be changed.
 #
 sub GroupContent
     {
@@ -148,47 +158,56 @@ sub GroupContent
 #
 #   Function: GroupIsEmpty
 #
-#   Returns whether the group is empty.
+#   If the type is <MENU_GROUP>, returns whether the group is empty.
 #
 sub GroupIsEmpty
-    {  return (!defined $_[0]->[TARGET]);  };
+    {
+    my $self = shift;
+    return (scalar @{$self->GroupContent()} > 0);
+    };
 
 
 #
 #   Function: PushToGroup
 #
-#   Adds a new <NaturalDocs::Menu::Entry> object to the end of the group content.
+#   Pushes the entry to the end of the group content.
 #
-#   Parameters:
-#
-#       item    - The <NaturalDocs::Menu::Entry> object to add to the group.
-#
-sub PushToGroup #(item)
+sub PushToGroup #(entry)
     {
-    my ($self, $item) = @_;
-
-    if (!defined $self->[TARGET])
-        {  $self->[TARGET] = [ ];  };
-
-    push @{$self->[TARGET]}, $item;
+    my ($self, $entry) = @_;
+    push @{$self->GroupContent()}, $entry;
     };
 
 
 #
-#   Function: PopFromGroup
+#   Function: DeleteFromGroup
 #
-#   Removes the last item from the group content.
+#   Deletes an entry from the group content by index.
 #
-sub PopFromGroup
+sub DeleteFromGroup #(index)
+    {
+    my ($self, $index) = @_;
+
+    my $groupContent = $self->GroupContent();
+
+    splice( @$groupContent, $index, 1 );
+    };
+
+
+#
+#   Function: MarkEndOfOriginal
+#
+#   If the group doesn't already have one, adds a <MENU_ENDOFORIGINAL> entry to the end and sets the
+#   <MENU_GROUP_HASENDOFORIGINAL> flag.
+#
+sub MarkEndOfOriginal
     {
     my $self = shift;
 
-    if (defined $self->[TARGET])
+    if (($self->Flags() & ::MENU_GROUP_HASENDOFORIGINAL()) == 0)
         {
-        pop @{$self->[TARGET]};
-
-        if (!scalar @{$self->[TARGET]})
-            {  $self->[TARGET] = undef;  };
+        $self->PushToGroup( NaturalDocs::Menu::Entry::New(::MENU_ENDOFORIGINAL(), undef, undef, undef) );
+        $self->SetFlags( $self->Flags() | ::MENU_GROUP_HASENDOFORIGINAL() );
         };
     };
 
