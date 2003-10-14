d14687b1bdc6dc4fad85e725c889f4f3dad9eb11
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2003-10-14 22:51:28 +0000

    Add SetBody() and change all the 'section' references to 'topic'.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@208 6b081834-2649-4b4f-abbc-426aeab4333c

--- Modules/NaturalDocs/Parser/ParsedTopic.pm
+++ Modules/NaturalDocs/Parser/ParsedTopic.pm
@@ -25,13 +25,13 @@ package NaturalDocs::Parser::ParsedTopic;
 #
 #   The object is a blessed arrayref with the following indexes.
 #
-#       TYPE           - The type of the section.  Will be one of the <Topic Types>.
-#       NAME          - The name of the section.
-#       CLASS        - The class of the section, if any.  This applies to Name only.  Will be undef if global.
-#       SCOPE        - The scope the section's body appears in.  This may be different from Class.  Will be undef if global.
+#       TYPE           - The type of the topic.  Will be one of the <Topic Types>.
+#       NAME          - The name of the topic.
+#       CLASS        - The class of the topic, if any.  This applies to Name only.  Will be undef if global.
+#       SCOPE        - The scope the topic's body appears in.  This may be different from Class.  Will be undef if global.
 #       PROTOTYPE - The prototype, if it exists and is applicable.
 #       SUMMARY    - The summary, if it exists.
-#       BODY          - The body of the section, formatted in <NDMarkup>.  Some sections may not have bodies, and if not, this
+#       BODY          - The body of the topic, formatted in <NDMarkup>.  Some topics may not have bodies, and if not, this
 #                           will be undef.
 #
 use constant TYPE => 0;
@@ -54,13 +54,13 @@ use constant BODY => 6;
 #
 #   Parameters:
 #
-#       type          - The type of the section.  Will be one of the <Topic Types>.
-#       name        - The name of the section.
-#       class         - The class of the section's _name_, if any.  Set to undef if global.
-#       scope        - The scope the section's _body_ appears in.  This may be different from class.  Set to undef if global.
+#       type          - The type of the topic.  Will be one of the <Topic Types>.
+#       name        - The name of the topic.
+#       class         - The class of the topic's _name_, if any.  Set to undef if global.
+#       scope        - The scope the topic's _body_ appears in.  This may be different from class.  Set to undef if global.
 #       prototype  - If the type is <TOPIC_FUNCTION> or <TOPIC_VARIABLE>, the prototype, if it exists.  Otherwise set to undef.
-#       summary  - The summary of the section, if any.
-#       body         - The body of the section, formatted in <NDMarkup>.  May be undef, as some sections may not have bodies.
+#       summary  - The summary of the topic, if any.
+#       body         - The body of the topic, formatted in <NDMarkup>.  May be undef, as some topics may not have bodies.
 #
 #   Returns:
 #
@@ -80,22 +80,22 @@ sub New #(type, name, class, scope, prototype, summary, body)
 
 
 # Function: Type
-# Returns the type of the section.  Will be one of <Topic Types>.
+# Returns the type of the topic.  Will be one of <Topic Types>.
 sub Type
     {  return $_[0]->[TYPE];  };
 
 # Function: Name
-# Returns the name of the section.
+# Returns the name of the topic.
 sub Name
     {  return $_[0]->[NAME];  };
 
 # Function: Class
-# Returns the class of the section.  Applies to <Name()> only.  Will be undef if global.
+# Returns the class of the topic.  Applies to <Name()> only.  Will be undef if global.
 sub Class
     {  return $_[0]->[CLASS];  };
 
 # Function: Scope
-# Returns the scope the section appears in.  Applies to <Body()> only.  Will be undef if global.
+# Returns the scope the topic appears in.  Applies to <Body()> only.  Will be undef if global.
 sub Scope
     {  return $_[0]->[SCOPE];  };
 
@@ -113,14 +113,22 @@ sub SetPrototype #(prototype)
     };
 
 # Function: Summary
-# Returns the section summary, if it exists, formatted in <NDMarkup>.
+# Returns the topic summary, if it exists, formatted in <NDMarkup>.
 sub Summary
     {  return $_[0]->[SUMMARY];  };
 
 # Function: Body
-# Returns the section's body, formatted in <NDMarkup>.  May be undef.
+# Returns the topic's body, formatted in <NDMarkup>.  May be undef.
 sub Body
     {  return $_[0]->[BODY];  };
+
+# Function: SetBody
+# Replaces the topic's body, formatted in <NDMarkup>.  May be undef.
+sub SetBody #(body)
+    {
+    my ($self, $body) = @_;
+    $self->[BODY] = $body;
+    };
 
 
 1;
