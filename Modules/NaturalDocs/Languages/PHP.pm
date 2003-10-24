9b91ea2c4a94ad3d175a0907eccbdb65888ff0f2
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2003-10-24 06:07:41 +0000

    Add explanation of escapement issue.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@229 6b081834-2649-4b4f-abbc-426aeab4333c

--- Modules/NaturalDocs/Languages/PHP.pm
+++ Modules/NaturalDocs/Languages/PHP.pm
@@ -44,4 +44,21 @@ sub MakeSortableSymbol #(name, type)
     };
 
 
+#
+#   About: Escapement
+#
+#   Okay, so Natural Docs has escapement detection now, so why isn't it implemented for PHP?  Well, I actually do have some
+#   (untested) code that does just that, but I didn't include it because PHP ignores closing escapement characters (like ?>) when
+#   they're in a multiline comment or a string.  Since I don't have the architecture in place for _that_ kind of specificity yet, I'd
+#   rather not include it at all yet.  It will have to wait for full code documentation when we can deal with that.
+#
+#   Still, why not include it anyway?  Because right now, everything, including the HTML, is treated as PHP code.  This has the risk
+#   of HTML looking like PHP comments with Natural Docs content and being interpreted as such.  With the existing escapement
+#   code, you have the risk of ending escapement prematurely, which would cause legitimate PHP code, and possibly
+#   documentation, to be ignored.  I see this scenario as much more probable than the HTML one, plus the result of including
+#   something inappropriate is much more benign than the result of excluding something appropriate.
+#
+
+
+
 1;
