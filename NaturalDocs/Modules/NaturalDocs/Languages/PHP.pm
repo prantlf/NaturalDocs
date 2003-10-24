8384e9a6159851f73e13e29e961d8b8260f72f90
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2003-10-24 05:11:39 +0000

    Add a custom PHP class for sorting.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk@227 6b081834-2649-4b4f-abbc-426aeab4333c

---
+++ NaturalDocs/Modules/NaturalDocs/Languages/PHP.pm
@@ -0,0 +1,47 @@
+###############################################################################
+#
+#   Class: NaturalDocs::Languages::PHP
+#
+###############################################################################
+#
+#   A subclass to handle the language variations of PHP.
+#
+###############################################################################
+
+# This file is part of Natural Docs, which is Copyright © 2003 Greg Valure
+# Natural Docs is licensed under the GPL
+
+use strict;
+use integer;
+
+package NaturalDocs::Languages::PHP;
+
+use base 'NaturalDocs::Languages::Language';
+
+
+#
+#   Topic: Inherits
+#
+#   <NaturalDocs::Languages::Language>
+#
+
+
+#
+#   Function: MakeSortableSymbol
+#
+#   PHP's variables start with dollar signs.  This function strips them off for sorting.
+#
+sub MakeSortableSymbol #(name, type)
+    {
+    my ($self, $name, $type) = @_;
+
+    if ($type == ::TOPIC_VARIABLE())
+        {
+        $name =~ s/^\$//;
+        };
+
+    return $name;
+    };
+
+
+1;
