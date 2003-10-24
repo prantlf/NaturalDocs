705820dcfce7a304f75c853278ca9200f853244c
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2003-10-24 04:54:45 +0000

    Add Perl class for custom sorting.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@225 6b081834-2649-4b4f-abbc-426aeab4333c

---
+++ Modules/NaturalDocs/Languages/Perl.pm
@@ -0,0 +1,48 @@
+###############################################################################
+#
+#   Class: NaturalDocs::Languages::Perl
+#
+###############################################################################
+#
+#   A subclass to handle the language variations of Perl.
+#
+###############################################################################
+
+# This file is part of Natural Docs, which is Copyright © 2003 Greg Valure
+# Natural Docs is licensed under the GPL
+
+use strict;
+use integer;
+
+package NaturalDocs::Languages::Perl;
+
+use base 'NaturalDocs::Languages::Language';
+
+
+#
+#   Topic: Inherits
+#
+#   <NaturalDocs::Languages::Language>
+#
+
+
+#
+#   Function: MakeSortableSymbol
+#
+#   Perl's variables start with symbols: $ for scalars, @ for arrays, and % for hashes.  This function strips them
+#   off for sorting.
+#
+sub MakeSortableSymbol #(name, type)
+    {
+    my ($self, $name, $type) = @_;
+
+    if ($type == ::TOPIC_VARIABLE())
+        {
+        $name =~ s/^[\$\@\%]//;
+        };
+
+    return $name;
+    };
+
+
+1;
