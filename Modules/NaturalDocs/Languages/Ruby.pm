c05ca816c4a629401ab697688b3377e32df6a3e8
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2003-12-26 15:16:49 +0000

    Add support for Ruby.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@268 6b081834-2649-4b4f-abbc-426aeab4333c

---
+++ Modules/NaturalDocs/Languages/Ruby.pm
@@ -0,0 +1,48 @@
+###############################################################################
+#
+#   Class: NaturalDocs::Languages::Ruby
+#
+###############################################################################
+#
+#   A subclass to handle the language variations of Ruby.
+#
+###############################################################################
+
+# This file is part of Natural Docs, which is Copyright © 2003 Greg Valure
+# Natural Docs is licensed under the GPL
+
+use strict;
+use integer;
+
+package NaturalDocs::Languages::Ruby;
+
+use base 'NaturalDocs::Languages::Language';
+
+
+#
+#   Topic: Inherits
+#
+#   <NaturalDocs::Languages::Language>
+#
+
+
+#
+#   Function: MakeSortableSymbol
+#
+#   Ruby's variables may start with symbols: none for local, $ for global, @ for instance, and @@ for class.  This function strips
+#   them off for sorting.
+#
+sub MakeSortableSymbol #(name, type)
+    {
+    my ($self, $name, $type) = @_;
+
+    if ($type == ::TOPIC_VARIABLE())
+        {
+        $name =~ s/^(?:\@\@|[\$\@])//;
+        };
+
+    return $name;
+    };
+
+
+1;
