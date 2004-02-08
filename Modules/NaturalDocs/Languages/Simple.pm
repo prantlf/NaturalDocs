5b4dea8b0099bfe1b6b79e0ebfffdad7599806e2
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2004-02-08 23:24:45 +0000

    Move parser implementation and support functions to ND:L:Simple.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@301 6b081834-2649-4b4f-abbc-426aeab4333c

--- Modules/NaturalDocs/Languages/Simple.pm
+++ Modules/NaturalDocs/Languages/Simple.pm
@@ -199,6 +199,157 @@ sub LineExtender
 
 
 #
+#   Function: ParseFile
+#
+#   Parses the passed source file, sending comments acceptable for documentation to <NaturalDocs::Parser->OnComment()>
+#   and all other sections to <OnCode()>.
+#
+#   Parameters:
+#
+#       sourceFile - The name of the source file to parse.
+#       topicList - A reference to the list of <NaturalDocs::Parser::ParsedTopics> being built by the file.
+#
+sub ParseFile #(sourceFile, topicsList)
+    {
+    my ($self, $sourceFile, $topicsList) = @_;
+
+    open(SOURCEFILEHANDLE, '<' . $sourceFile)
+        or die "Couldn't open input file " . $sourceFile . "\n";
+
+    my @commentLines;
+    my @codeLines;
+    my $lastCommentTopicCount = 0;
+
+    if ($self->FileIsComment())
+        {
+        my $line;
+
+        while ($line = <SOURCEFILEHANDLE>)
+            {
+            ::XChomp(\$line);
+            push @commentLines, $line;
+            };
+
+        NaturalDocs::Parser->OnComment(\@commentLines, 1);
+        }
+
+    else
+        {
+        my $line = <SOURCEFILEHANDLE>;
+        my $lineNumber = 1;
+
+        while (defined $line)
+            {
+            ::XChomp(\$line);
+            my $originalLine = $line;
+
+
+            # Retrieve single line comments.  This leaves $line at the next line.
+
+            if ($self->StripOpeningSymbol(\$line, $self->LineCommentSymbols()))
+                {
+                do
+                    {
+                    push @commentLines, $line;
+                    $line = <SOURCEFILEHANDLE>;
+
+                    if (!defined $line)
+                        {  goto EndDo;  };
+
+                    ::XChomp(\$line);
+                    }
+                while ($self->StripOpeningSymbol(\$line, $self->LineCommentSymbols()));
+
+                EndDo:  # I hate Perl sometimes.
+                }
+
+
+            # Retrieve multiline comments.  This leaves $line at the next line.
+
+            elsif ($self->StripOpeningSymbol(\$line, $self->OpeningCommentSymbols()))
+                {
+                # Note that it is possible for a multiline comment to start correctly but not end so.  We want those comments to stay in
+                # the code.  For example, look at this prototype with this splint annotation:
+                #
+                # int get_array(integer_t id,
+                #                    /*@out@*/ array_t array);
+                #
+                # The annotation starts correctly but doesn't end so because it is followed by code on the same line.
+
+                my ($symbol, $lineRemainder);
+
+                for (;;)
+                    {
+                    ($symbol, $lineRemainder) = $self->StripClosingSymbol(\$line, $self->ClosingCommentSymbols());
+
+                    push @commentLines, $line;
+
+                    #  If we found an end comment symbol...
+                    if (defined $symbol)
+                        {  last;  };
+
+                    $line = <SOURCEFILEHANDLE>;
+
+                    if (!defined $line)
+                        {  last;  };
+
+                    ::XChomp(\$line);
+                    };
+
+                if ($lineRemainder !~ /^[ \t]*$/)
+                    {
+                    # If there was something past the closing symbol this wasn't an acceptable comment, so move the lines to code.
+                    push @codeLines, @commentLines;
+                    @commentLines = ( );
+                    };
+
+                $line = <SOURCEFILEHANDLE>;
+                }
+
+
+            # Otherwise just add it to the code.
+
+            else
+                {
+                push @codeLines, $line;
+                $line = <SOURCEFILEHANDLE>;
+                };
+
+
+            # If there were comments, send them to Parser->OnComment().
+
+            if (scalar @commentLines)
+                {
+                # First process any code lines before the comment.
+                if (scalar @codeLines)
+                    {
+                    $self->OnCode(\@codeLines, $lineNumber, $topicsList, $lastCommentTopicCount);
+                    $lineNumber += scalar @codeLines;
+                    @codeLines = ( );
+                    };
+
+                $lastCommentTopicCount = NaturalDocs::Parser->OnComment(\@commentLines, $lineNumber);
+                $lineNumber += scalar @commentLines;
+                @commentLines = ( );
+                };
+
+            };  # while (defined $line)
+
+
+        # Clean up any remaining code.
+        if (scalar @codeLines)
+            {
+            $self->OnCode(\@codeLines, $lineNumber, $topicsList, $lastCommentTopicCount);
+            @codeLines = ( );
+            };
+
+        };
+
+    close(SOURCEFILEHANDLE);
+    };
+
+
+#
 #   Function: OnCode
 #
 #   Called whenever a section of code is encountered by the parser.  Is used to find the prototype of the last topic created.
@@ -260,6 +411,11 @@ sub OnCode #(codeLines, codeLineNumber, topicList, lastCommentTopicCount)
         # If we got out of that while loop by running out of lines, there was no prototype.
         };
     };
+
+
+
+###############################################################################
+# Group: Support Functions
 
 
 #
@@ -340,8 +496,88 @@ sub RemoveExtenders #(stringRef)
     };
 
 
-###############################################################################
-# Group: Support Functions
+#
+#   Function: StripOpeningSymbol
+#
+#   Determines if the line starts with any of the passed symbols, and if so, replaces it with spaces.  This only happens
+#   if the only thing before it on the line is whitespace.
+#
+#   Parameters:
+#
+#       lineRef - A reference to the line to check.
+#       symbols - An arrayref of the symbols to check for.
+#
+#   Returns:
+#
+#       If the line starts with any of the passed comment symbols, it will replace it in the line with spaces and return the symbol.
+#       If the line doesn't, it will leave the line alone and return undef.
+#
+sub StripOpeningSymbol #(lineRef, symbols)
+    {
+    my ($self, $lineRef, $symbols) = @_;
+
+    if (!defined $symbols)
+        {  return undef;  };
+
+    foreach my $symbol (@$symbols)
+        {
+        my $index = index($$lineRef, $symbol);
+
+        if ($index != -1 && substr($$lineRef, 0, $index) =~ /^[ \t]*$/)
+            {
+            return substr($$lineRef, $index, length($symbol), ' ' x length($symbol));
+            };
+        };
+
+    return undef;
+    };
+
+
+#
+#   Function: StripClosingSymbol
+#
+#   Determines if the line contains a symbol, and if so, truncates it just before the symbol.
+#
+#   Parameters:
+#
+#       lineRef - A reference to the line to check.
+#       symbols - An arrayref of the symbols to check for.
+#
+#   Returns:
+#
+#       The array ( symbol, lineRemainder ), or undef if the symbol was not found.
+#
+#       symbol - The symbol that was found.
+#       lineRemainder - Everything on the line following the symbol.
+#
+sub StripClosingSymbol #(lineRef, symbols)
+    {
+    my ($self, $lineRef, $symbols) = @_;
+
+    my $index = -1;
+    my $symbol;
+
+    foreach my $testSymbol (@$symbols)
+        {
+        my $testIndex = index($$lineRef, $testSymbol);
+
+        if ($testIndex != -1 && ($index == -1 || $testIndex < $index))
+            {
+            $index = $testIndex;
+            $symbol = $testSymbol;
+            };
+        };
+
+    if ($index != -1)
+        {
+        my $lineRemainder = substr($$lineRef, $index + length($symbol));
+        $$lineRef = substr($$lineRef, 0, $index);
+
+        return ($symbol, $lineRemainder);
+        }
+    else
+        {  return undef;  };
+    };
 
 
 #
