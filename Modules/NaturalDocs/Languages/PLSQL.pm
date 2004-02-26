0619086ab12ed65ba6f863a33616619fe0da902a
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2004-02-26 18:54:34 +0000

    Add support for Oracle syntax, as well as the added parenthesis and comma enders.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@356 6b081834-2649-4b4f-abbc-426aeab4333c

--- Modules/NaturalDocs/Languages/PLSQL.pm
+++ Modules/NaturalDocs/Languages/PLSQL.pm
@@ -22,12 +22,30 @@ use base 'NaturalDocs::Languages::Simple';
 #
 #   Function: EndOfPrototype
 #
+#   Oracle 8.1's object oriented extensions allow functions and variables to end with a comma or a closing parenthesis.
+#
+#   (start code)
+#
+#   CREATE OR REPLACE TYPE obj_Test AS OBJECT (
+#       dummy NUMBER,
+#       dummy2 VARCHAR2,
+#       MEMBER FUNCTION test( p_lala IN NUMBER ) RETURN NUMBER,
+#       MEMBER FUNCTION test_2 RETURN NUMBER,
+#       MEMBER PROCEDURE test_3( p_lala IN NUMBER, p_lala2 IN NUMBER, p_lala3 OUT VARCHAR2 )
+#   ) NOT FINAL;
+#
+#   (end code)
+#
+#   Both symbols are included as enders.  We need to generate false positives for commas that appear within parenthesis, as
+#   well as for the closing parenthesis if an opening one is present.
+#
 #   Microsoft's SQL specifies parameters as shown below.
 #
 #   > CREATE PROCEDURE Test @as int, @foo int AS ...
 #
-#   Having a parameter @is or @as is perfectly valid even though those words are also used to end the prototype.  Note any
-#   false positives created by this.
+#   Having a parameter @is or @as is perfectly valid even though those words are also used to end the prototype.  We need to
+#   generate false positives created by this.  Also note that it does not have parenthesis for variable lists.  We generate false
+#   positives for all commas if the prototype doesn't have parenthesis but does have @ characters.
 #
 sub EndOfPrototype #(type, stringRef, falsePositives)
     {
@@ -39,22 +57,69 @@ sub EndOfPrototype #(type, stringRef, falsePositives)
         {
         $falsePositives = { };
 
+
+        # Generate false positives for @keyword enders.
+
         foreach my $ender (@{$self->FunctionEnders()})
             {
-            my $index = 0;
+            if ($ender =~ /^[a-z]/i)
+                {
+                my $index = 0;
+
+                for (;;)
+                    {
+                    $index = index($$stringRef, '@' . $ender, $index);
+
+                    if ($index == -1)
+                        {  last;  };
+
+                    # +1 because the positive will be after the @ symbol.
+                    $falsePositives->{$index + 1} = 1;
+                    $index++;
+                    };
+                };
+            };
+
+
+        # Generate a false positive for the closing parenthesis if there is an opening one.
+
+        my $openingParenthesis = index($$stringRef, '(');
+
+        if ($openingParenthesis != -1)
+            {
+            my $closingParenthesis = index($$stringRef, ')', $openingParenthesis);
+
+            if ($closingParenthesis != -1)
+                {  $falsePositives->{$closingParenthesis} = 1;  };
+
+
+            # Also generate false positives for all commas appearing inside the parenthesis.
+
+            my $index = $openingParenthesis;
 
             for (;;)
                 {
-                $index = index($$stringRef, '@' . $ender, $index);
+                $index = index($$stringRef, ',', $index + 1);
 
-                if ($index == -1)
+                if ($index == -1 || ($closingParenthesis != -1 && $index > $closingParenthesis))
                     {  last;  };
 
-                # +1 because the positive will be after the @ symbol.
-                $falsePositives->{$index + 1} = 1;
-                $index++;
+                $falsePositives->{$index} = 1;
+                };
+            }
+
+        # If there are no parenthesis and the prototype contains an @ character, all commas are false positives.
+        elsif (index($$stringRef, '@') != -1)
+            {
+            my $index = index($$stringRef, ',');
+
+            while ($index != -1)
+                {
+                $falsePositives->{$index} = 1;
+                $index = index($$stringRef, ',', $index + 1);
                 };
             };
+
 
         if (!scalar keys %$falsePositives)
             {  $falsePositives = undef;  };
