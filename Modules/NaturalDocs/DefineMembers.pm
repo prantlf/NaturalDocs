557c537508c3263b30447b3592dfcf7538c7a6d9
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2004-03-11 21:28:11 +0000

    Update copyright.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk/NaturalDocs@373 6b081834-2649-4b4f-abbc-426aeab4333c

--- Modules/NaturalDocs/DefineMembers.pm
+++ Modules/NaturalDocs/DefineMembers.pm
@@ -24,7 +24,7 @@
 #
 ###############################################################################
 
-# This file is part of Natural Docs, which is Copyright © 2004 Greg Valure
+# This file is part of Natural Docs, which is Copyright © 2003-2004 Greg Valure
 # Natural Docs is licensed under the GPL
 
 
