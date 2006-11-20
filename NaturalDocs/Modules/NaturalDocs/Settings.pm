77a4d5fa37630d041f21c3cd3854435b8e43a867
gregvalure <gregvalure@6b081834-2649-4b4f-abbc-426aeab4333c>
2006-11-20 04:17:59 +0000

    New development release.
    git-svn-id: svn://svn.code.sf.net/p/naturaldocs/code/trunk@854 6b081834-2649-4b4f-abbc-426aeab4333c

--- NaturalDocs/Modules/NaturalDocs/Settings.pm
+++ NaturalDocs/Modules/NaturalDocs/Settings.pm
@@ -564,7 +564,7 @@ sub AppVersion
 sub TextAppVersion
     {
     # return '1.35';
-    return 'Development Release 07-30-2006 (1.35 base)';
+    return 'Development Release 11-19-2006 (1.35 base)';
     };
 
 #
