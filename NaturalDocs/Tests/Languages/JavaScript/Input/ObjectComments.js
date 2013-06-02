70c95447f86e3c23dc9a221975c3e1349aab8c5b
Ferdinand Prantl <prantlf@gmail.com>
2013-06-02 13:08:16 +0200

    Add 'function object' as a class-type keyword

---
+++ NaturalDocs/Tests/Languages/JavaScript/Input/ObjectComments.js
@@ -0,0 +1,16 @@
+/**
+ * Function Object: ObjectCommentsFunction1
+ *
+ * The first function object.
+ */
+
+/**
+ * The first variable.
+ */
+var ObjectCommentsVar1;
+
+/**
+ * The constructor of the first function object.
+ */
+function ObjectCommentsFunction1() {
+}
